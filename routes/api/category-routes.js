1ab8b9d359059b757512028d24e2945b003d4283
mjhwest89 <mjhwest89@gmail.com>
2021-12-02 02:11:37 +1030

    got post route working for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, ProductTag } = require('../../models');
 
 // The `/api/categories` endpoint
 //working
@@ -14,13 +14,51 @@ router.get('/', async(req, res) => {
     }
 })
 
-router.get('/:id', (req, res) => {
-    // find one category by its `id` value
-    // be sure to include its associated Products
+
+// find one category by its `id` value
+// be sure to include its associated Products
+router.get('/:id', async(req, res) => {
+    try {
+        const categoryData = await Category.findByPk(req.params.id, {
+            include: [{ model: Product }]
+        });
+        if (!categoryData) {
+            res.status(404).json({ message: 'No category with that id!' });
+            return;
+        }
+        res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
+    // Category.create(req.body)
+    try {
+        const categoryData = await Category.create(req.body);
+        res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+    //     .then((category) => {
+    //         if (req.body.category_name) {
+    //             const catName = req.body.category_name.map((category_name) => {
+    //                 return {
+    //                     category_name
+    //                 };
+    //             });
+    //             return ProductTag.bulkCreate(catName);
+    //         }
+    //         res.status(200).json(category);
+    //     }).then((cats) => res.stauts(200).json(cats))
+    //     .catch((err) => {
+    //         console.log(err);
+    //         res.status(400).json(err)
+    //     })
     // create a new category
+    // {
+    //   'category_name': "Belts"
+    // }
 });
 
 router.put('/:id', (req, res) => {
