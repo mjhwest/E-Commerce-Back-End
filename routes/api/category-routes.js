465a45673ecfdf77882e7d4f3b1b57e917608465
mjhwest89 <mjhwest89@gmail.com>
2021-12-02 02:18:31 +1030

    got delete route working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -65,8 +65,25 @@ router.put('/:id', (req, res) => {
     // update a category by its `id` value
 });
 
+//delete is working 
 router.delete('/:id', (req, res) => {
     // delete a category by its `id` value
+    Category.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(categoryData => {
+            if (!categoryData) {
+                res.status(404).json({ message: 'No category found with this id' });
+                return;
+            }
+            res.json(categoryData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
