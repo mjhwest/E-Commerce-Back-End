2fbd22683ca4871a9060f2432c4cff6c196071e6
mjhwest89 <mjhwest89@gmail.com>
2021-12-02 11:59:36 +1030

    get / working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,36 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// find all tags
+// be sure to include its associated Product data
+//working 
+router.get('/', async(req, res) => {
+    try {
+        const tagData = await Tag.findAll({
+            include: [{ model: Product }]
+        });
+        res.status(200).json(tagData);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+    // find a single tag by its `id`
+    // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+    // create a new tag
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+    // update a tag's name by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+    // delete on tag by its `id` value
 });
 
 module.exports = router;
